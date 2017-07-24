d9e0ccdfcb74b5db4ed552abaa3fb58dc7c8ba0f
Joe Garber <joegarb@users.noreply.github.com>
2017-07-23 20:09:54 -0400

    Basic get and post API endpoints for rsvp

--- server.js
+++ server.js
@@ -1,30 +1,85 @@
 'use strict';
 
+const port = process.env.API_PORT || 8080;
+const mongoUrl = process.env.MONGO_URL;
+
 const express = require('express');
-const bodyParser= require('body-parser');
+const bodyParser = require('body-parser');
 const mongo = require('mongodb').MongoClient;
 const app = express();
+const winston = require('winston');
+const morgan = require('morgan');
+const moment = require('moment');
 
-const port = process.env.API_PORT || 8080;
+var logger = new winston.Logger({
+    transports: [
+        new winston.transports.File({
+            level: 'info',
+            filename: './logger.log',
+            handleExceptions: true,
+            json: true,
+            maxsize: 5242880, // 5MB
+            maxFiles: 5,
+            colorize: false,
+            timestamp: () => {
+                return moment().format();
+            }
+        }),
+        new winston.transports.Console({
+            level: 'debug',
+            handleExceptions: true,
+            json: false,
+            colorize: true,
+            timestamp: () => {
+                return moment().format();
+            }
+        })
+    ],
+    exitOnError: false
+});
+logger.stream = {
+    write: (message, encoding) => {
+        logger.info(message);
+    }
+};
 
+app.use(morgan(':remote-addr :method :url :status :res[content-length] - :response-time ms', {'stream': logger.stream}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-/*mongo.connect('link-to-mongodb', (err, database) => {
+let db;
+mongo.connect(mongoUrl, (err, database) => {
     if (err) {
-        console.log(err);
-        return;
+        logger.error(err);
+    } else {
+        logger.info('Connected to database');
+        db = database;
     }
-    db = database;
-    app.listen(3000, () => {
-        console.log('Listening on port 3000');
-    });
-});*/
+});
 
-// app.get('/api', (req, res) => {
-//     res.send('This is the API');
-// });
+app.get('/rsvp', (req, res) => {
+    db.collection('rsvp').find().toArray((err, results) => {
+        if (err) {
+            logger.error(err);
+            res.send(err);
+        } else {
+            res.send(results);
+        }
+    });
+});
+
+app.post('/rsvp', (req, res) => {
+    logger.info('POST /rsvp', req.body);
+    db.collection('rsvp').save(req.body, (err, result) => {
+        if (err) {
+            logger.error(err);
+            res.send(err);
+        } else {
+            res.send(result);
+        }
+    });
+});
 
 app.listen(port, () => {
-    console.log('Listening on port ' + port);
+    logger.info('Listening on port ' + port);
 });
