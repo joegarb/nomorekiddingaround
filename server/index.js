0f71cbb8b81ae7fc2b6d7ee389da73a301723f1f
Joe Garber <joegarb@users.noreply.github.com>
2017-08-26 22:59:51 -0400

    server.js -> server/index.js

--- server/index.js
+++ server/index.js
@@ -6,15 +6,16 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const morgan = require('morgan');
-const logger = require('./server/logger');
+const logger = require('./logger');
+const path = require('path');
 
 app.enable('trust proxy');
 app.use(morgan(':remote-addr :method :url :status :res[content-length] - :response-time ms', {'stream': logger.stream}));
-app.use(express.static('dist'));
+app.use(express.static(path.resolve(__dirname, '..', 'dist')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.use(require('./server/routes'));
+app.use(require('./routes'));
 
 app.listen(port, () => {
     logger.info('Listening on port ' + port);
