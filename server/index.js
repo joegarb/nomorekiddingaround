8deae43cbbd7464b7d994cf6d8286d2f86daab5a
Joe Garber <joegarb@users.noreply.github.com>
2018-05-02 21:06:00 -0400

    Redirect www to non-www

--- server/index.js
+++ server/index.js
@@ -10,6 +10,16 @@ const logger = require('./logger');
 const path = require('path');
 
 app.enable('trust proxy');
+
+// Redirect www to non-www
+app.use((req, res, next) => {
+    if (req.headers.host.slice(0, 4) === 'www.') {
+        var newHost = req.headers.host.slice(4);
+        return res.redirect(301, req.protocol + '://' + newHost + req.originalUrl);
+    }
+    next();
+});
+
 app.use(morgan(':remote-addr :method :url :status :res[content-length] - :response-time ms', {'stream': logger.stream}));
 app.use(express.static(path.resolve(__dirname, '..', 'dist')));
 app.use(bodyParser.json());
